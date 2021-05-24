f9bcf84842439ced4708cc18acde44499f360ad0
Stas Ivanovskii <ivanovskiy999@gmail.com>
2021-05-25 09:48:59 +1000

    Add index_page | bootstrap5 support

--- math_abiturient/settings.py
+++ math_abiturient/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- math_abiturient/urls.py
+++ math_abiturient/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Не кэшировать статику браузером в дебаге
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve))) 
