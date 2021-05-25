96d8e3d230617166e923150b9b84f5cc6c33fd54
Stas Ivanovskii <ivanovskiy999@gmail.com>
2021-05-25 11:39:40 +1000

    Add login/logout/profile pages

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index
 
+from .views import (
+    index, profile,
+    MALoginView, MALogoutView
+)
 
 urlpatterns = [
+    path('accounts/login/', MALoginView.as_view(), name='login'),
+    path('accounts/logout/', MALogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('', index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
 def index(request):
     return render(request, 'main/index.html')
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class MALoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+class MALogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
