3f350ad1f3dff3725b93de7a888c3ddbe5e6f460
Stas Ivanovskii <ivanovskiy999@gmail.com>
2021-05-25 11:38:05 +1000

    Expend User model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class AdvUser(AbstractUser):
+    image = models.ImageField(upload_to='')
+    biography = models.TextField(blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
