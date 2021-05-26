695c91717af71ce5eb8629c4e2fef1ec2ce83b8b
Stas Ivanovskii <ivanovskiy999@gmail.com>
2021-05-26 17:10:43 +1000

    Fix bugs

--- main/utilities.py
+++ main/utilities.py
@@ -8,7 +8,7 @@ def send_activation_notification(user):
     if ALLOWED_HOSTS and '*' not in ALLOWED_HOSTS:
         host = 'http://' + ALLOWED_HOSTS[0]
     else:
-        host = 'http://localhost:8000'
+        host = 'http://127.0.0.1:8000'
     context = {'user': user, 'host': host, 'sign': signer.sign(user.username)}
     subject = render_to_string('email/activation_letter_subject.txt', context)
     body_text = render_to_string('email/activation_letter_body.txt', context)
