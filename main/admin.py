682cd0d611e5db7db9b9cc95090cf10592d2a0d4
Stas Ivanovskii <ivanovskiy999@gmail.com>
2021-06-16 10:29:23 +1000

    Expand Task, create FavoriteTask models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AdvUser, Task
+from .models import AdvUser, Task, FavoriteTask
 
 admin.site.register(AdvUser)
 admin.site.register(Task)
+admin.site.register(FavoriteTask)
--- main/models.py
+++ main/models.py
@@ -15,15 +15,46 @@ class AdvUser(AbstractUser):
     image = models.ImageField(upload_to='users/%Y/%m-%d/', blank=True,
                               verbose_name='Фотография профиля')
     biography = models.TextField(blank=True, verbose_name='О себе')
+    
+    @property
+    def get_published_tasks(self):
+        return Task.objects.filter(creator=self, is_published=True)
+
+    @property
+    def get_unpublished_tasks(self):
+        return Task.objects.filter(creator=self, is_published=False)
 
     class Meta(AbstractUser.Meta):
         pass
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=50)
-    content = MDTextField()
-    creator = models.ForeignKey(AdvUser, on_delete=models.CASCADE, null=True)
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Task(TimeStampMixin):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    description = models.CharField(max_length=500, blank=True, verbose_name='Описание')
+    content = MDTextField(verbose_name='Содержание')
+    creator = models.ForeignKey(AdvUser, on_delete=models.CASCADE)
+    published_at = models.DateTimeField(blank=True, null=True)
+    is_published = models.BooleanField(default=False, db_index=True,
+                                       verbose_name='Опубликовано?')
+
+    def __str__(self):
+        return f"[{self.creator}]: {self.title}"
+
+    def get_favorite_count(self):
+        return self.favoritetask_set.all().count()
+
+    class Meta:
+        ordering = ['-published_at', ]
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
 
 
 # Signal that removes task images from server
@@ -40,3 +71,24 @@ def task_images_delete(sender, instance, **kwargs):
             os.remove(os.path.join(MDEDITOR_IMAGE_FOLDER_PATH, image))
         except:
             continue
+
+
+class FavoriteBase(models.Model):
+    user = models.ForeignKey(AdvUser, verbose_name="Пользователь",
+                                      on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        abstract = True
+
+
+class FavoriteTask(FavoriteBase):
+    obj = models.ForeignKey(Task, verbose_name="Закладка",
+                                  on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "favorite_task"
+        verbose_name = 'Избранная задача'
+        verbose_name_plural = 'Избранные задачи'
